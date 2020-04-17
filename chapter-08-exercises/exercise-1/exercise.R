f37103cafbf928e08c1deba5bf79fff531e1c08a
paolopan@uw.edu <paolopan@uw.edu>
2020-04-16 19:32:21 -0700

    finish exercise 8

--- chapter-08-exercises/exercise-1/exercise.R
+++ chapter-08-exercises/exercise-1/exercise.R
@@ -1,28 +1,32 @@
 # Exercise 1: creating and accessing lists
 
 # Create a vector `my_breakfast` of everything you ate for breakfast
-
+my_breakfast <-c("milk", "bread")
 
 # Create a vector `my_lunch` of everything you ate (or will eat) for lunch
-
+my_lunch <- c("cwater", "fried rice")
 
 # Create a list `meals` that has contains your breakfast and lunch
-
+meals <- list(breakfast=my_breakfast, lunch=my_lunch)
 
 # Add a "dinner" element to your `meals` list that has what you plan to eat 
 # for dinner
+meals$dinner <- c("beef", "rice")
 
 
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
-
+dinner <- meals$dinner
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason beyond practice)
-
+meals[[5]] <- meals[["lunch"]]
 
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early_meals`
+#early_meals <- meals["breakfast"]
+#early_meals$lunch <- meals["lunch"]
+early_meals <- meals[1:2] 
 
 
 
@@ -30,11 +34,17 @@
 
 # Create a list that has the number of items you ate for each meal
 # Hint: use the `lappy()` function to apply the `length()` function to each item
-
+breakfast_item <- lapply(my_breakfast, length)
+lunch_item <- lapply(my_lunch, length)
+dinner_item <- lapply(dinner, length)
+items <- lapply(meals, length)
 
 # Write a function `add_pizza` that adds pizza to a given meal vector, and
 # returns the pizza-fied vector
-
+add_pizza <- function(m){
+  result <- c(m, "pizza")
+  return(result)
+}
 
 # Create a vector `better_meals` that is all your meals, but with pizza!
-
+better_meals <- lapply(meals, add_pizza)
--- chapter-08-exercises/exercise-2/exercise.R
+++ chapter-08-exercises/exercise-2/exercise.R
@@ -2,41 +2,52 @@
 
 # Create a *list* of 10 random numbers. Use the `runif()` function to make a 
 # vector of random numbers, then use `as.list()` to convert that to a list
-
+ran_num <- runif(10,0,10)
+ran_num <- as.list(ran_num)
 
 # Use `lapply()` to apply the `round()` function to each number, rounding it to 
 # the nearest 0.1 (one decimal place)
-
+rounded <- lapply(ran_num, round)
 
 # Create a variable 'sentence' that contains a sentence of text (something 
 # longish). Make the sentence lowercase; you can use a function to help.
-
+sentence <- "info201 is a tough class"
 
 # Use the `strsplit()` function to split the sentence into a vector of letters.
 # Hint: split on `""` to split every character
 # Note: this will return a _list_ with 1 element (which is the vector of letters)
-
+str_array <- strsplit(sentence,"")
 
 # Extract the vector of letters from the resulting list
-
+letters <- str_array[[1]]
 
 # Use the `unique()` function to get a vector of unique letters
-
+unique_letters <- unique(letters)
 
 # Define a function `count_occurrences` that takes in two parameters: a letter 
 # and a vector of letters. The function should return how many times that letter
 # occurs in the provided vector.
 # Hint: use a filter operation!
+count_occurrences <- function(letter, vector){
+  vector_of_same_letter <- vector[vector == letter] #vectorized operation
+  result <- length(vector_of_same_letter)
+  return(result)
+}
 
 
 # Call your `count_occurrences()` function to see how many times the letter 'e'
 # is in your sentence.
+count_occurrences("a", letters)
 
 
 # Use `sapply()` to apply your `count_occurrences()` function to each unique 
 # letter in the vector to determine their frequencies.
 # Convert the result into a list (using `as.list()`).
+count <- sapply(unique_letters, count_occurrences, letters)
+frequencies <- as.list(count)
 
 
 # Print the resulting list of frequencies
+print(frequencies)
+
 
