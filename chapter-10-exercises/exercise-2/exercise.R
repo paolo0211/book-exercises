fe6fcc534e31614f4caf77908afef588dc042b96
paolopan@uw.edu <paolopan@uw.edu>
2020-04-23 23:57:28 -0700

    finish exercise from chapter 10

--- chapter-10-exercises/exercise-2/exercise.R
+++ chapter-10-exercises/exercise-2/exercise.R
@@ -3,27 +3,30 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
+employee <- paste("Employee", 1:100)
 
 
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
-
+salary <- runif(100,40000,50000)
 
 # Create a vector of 100 annual salary adjustments between -5000 and 10000.
 # (A negative number represents a salary decrease due to corporate greed)
 # Again use the `runif()` function to pick 100 random numbers in that range.
-
+salary_adjustment <- runif(100,-5000,10000)
 
 # Create a data frame `salaries` by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salaries <- data.frame(employee, salary, salary_adjustment, stringsAsFactors = FALSE)
 
 # Add a column to the `salaries` data frame that represents each person's
 # salary in 2018 (e.g., with the salary adjustment added in).
+salaries$adjusted_salary <- salary + salary_adjustment
 
 
 # Add a column to the `salaries` data frame that has a value of `TRUE` if the 
 # person got a raise (their salary went up)
+salaries$raised <- adjusted_salary > salary
 
 
 
@@ -32,31 +35,32 @@
 ### cell rather than the whole row!)
 
 # What was the 2018 salary of Employee 57
-
+employee_57 <- salaries$adjusted_salary[57]
 
 # How many employees got a raise?
-
+num_raised <- length(salaries$employee[salaries$raised])
 
 # What was the dollar value of the highest raise?
-
+highest_raise <- max(salary_adjustment)
 
 # What was the "name" of the employee who received the highest raise?
-
+name <- salaries[salaries$salary_adjustment == highest_raise, "employee"]
 
 # What was the largest decrease in salaries between the two years?
-
+highest_decrease <- min(salary_adjustment)
 
 # What was the name of the employee who recieved largest decrease in salary?
-
+name_decrease <- salaries[salaries$salary_adjustment == highest_decrease, "employee"]
 
 # What was the average salary change?
-
+average_change <- mean(salary_adjustment)
 
 # For people who did not get a raise, how much money did they lose on average?
-
+average_lose <- mean(salaries$salary_adjustment[salaries$salary_adjustment <= 0])
 
 ## Consider: do the above averages match what you expected them to be based on 
 ## how you generated the salaries?
+#yes it is very close
 
 # Write a .csv file of your salary data to your working directory
-
+write.csv(salaries, "salaries", row.names = FALSE)
--- chapter-10-exercises/exercise-3/exercise.R
+++ chapter-10-exercises/exercise-3/exercise.R
@@ -2,43 +2,50 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data(USPersonalExpenditure)
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+personal_expenditure <- data.frame(USPersonalExpenditure, stringsAsFactors = FALSE)
 
 # What are the column names of your dataframe?
-
+column <- colnames(personal_expenditure)
 
 ## Consider: why are they so strange? Think about whether you could use a number 
 ## like 1940 with dollar notation!
 
-# What are the row names of your dataframe?
 
+# What are the row names of your dataframe?
+row <- rownames(personal_expenditure)
 
 # Add a column "category" to your data frame that contains the rownames
-
+personal_expenditure$category <- row
 
 # How much money was spent on personal care in 1940?
-
+personal_care <- personal_expenditure["Personal Care", "X1940"]
 
 # How much money was spent on Food and Tobacco in 1960?
-
+food_and_tobacco <- personal_expenditure["Food and Tobacco", "X1960"]
 
 # What was the highest expenditure category in 1960?
-
+highest_category <- max(personal_expenditure$X1960)
 
 # Define a function `lowest_category` that takes in a year as a parameter, and
 # returns the lowest spending category of that year
+lowest_category <- function(year){
+  year <- paste0("X",year)
+  result <- personal_expenditure$category[personal_expenditure[,year] == 
+                                   min(personal_expenditure[,year])]
+  return(result)
+}
 
 
 # Using your function, determine the lowest spending category of each year
 # Hint: use the `sapply()` function to apply your function to a vector of years
-
+lowest_spending_categories <- sapply(seq(1940,1960,5), lowest_category)
